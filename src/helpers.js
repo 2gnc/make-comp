dc96c525c6c7b8904d1c5d4670fe94310a51fbb4
2gnc <ksenia.v.polyakova@mail.ru>
2018-08-30 12:47:39 +0300

    добавлен хелпер создания директории и лога ошибки

--- src/helpers.js
+++ src/helpers.js
@@ -16,6 +16,27 @@ const _getTemplateText = (componentName, type) => {
 
 };
 
+const _makeFolder = (path, folderName) => {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(`${path}/${folderName}`, (err) => {
+            if (!err) resolve(`${path}/${folderName}`);
+            else reject(err);
+        })
+    })
+};
+
+const _logError = (errCode, otherParams) => {
+    const additionalInfo = otherParams ? ` ${otherParams.toString()}` : '';
+    switch (errCode) {
+        case 'EEXIST':
+            console.log(`${message.DIRECTORY_ALREADY_EXIST}${additionalInfo}`);
+            break;
+        default:
+            console.log(`${message.UNKNOWN_ERROR}${additionalInfo}`);
+            break;
+    }
+};
+
 const getFileText = async (componentName, type, translation) => {
     const template = await _getTemplateText(componentName, type);
     const translationString = translation ? `import * as i18n from ' b:components_${componentName} t:i18n';` : '';
@@ -44,5 +65,16 @@ const getRoot = (startPath) => {
 };
 
 
+const makeFolder = (path, folderName) => {
+    return new Promise(() => {     //TODO что тут делать с resolve, reject
+        _makeFolder(path, folderName)
+            .then((res) => console.log(`Создана директория: ${res}`))
+            .catch(e => _logError(e.code, folderName));
+    })
+};
+
+
+
 module.exports.getFileText = getFileText;
 module.exports.getRoot = getRoot;
+module.exports.makeFolder = makeFolder;
--- src/make-comp.js
+++ src/make-comp.js
@@ -1,4 +1,8 @@
-const { getFileText, getRoot } = require('./helpers');
+const {
+    getFileText,
+    getRoot,
+    makeFolder
+} = require('./helpers');
 
 module.exports = (args) => {
 
@@ -7,19 +11,19 @@ module.exports = (args) => {
             getFileText('MyPerfectComponent', 'visual-js', true),
             getFileText('MyPerfectComponent', 'visual-css'),
             getFileText('MyPerfectComponent', 'visual-md'),
+            getFileText('MyPerfectComponent', 'container'),
+            getFileText('MyPerfectComponent', 'translation'),
         ])
         .then((res) => {
             console.log(res[0]);
-            console.log(res[1]);
-            console.log(res[2]);
-            console.log(res[3]);
+            makeFolder(res[0], 'MyPerfectComponent')
+
         })
         .catch(e => e);
 };
 
 
-
-// TODO сделвать логику для отображения <<TRANSLATION>>
+// TODO при досоздании перевода, добавлять импорт в визуальный компонент
 // TODO сделать команду --help (выводит описание и подсказки)
 // TODO сделать команду config visual src/components
 // TODO сделать команду config container src/containers
@@ -29,4 +33,16 @@ module.exports = (args) => {
 // если вызвать make-comp Test без аргументов, то сначала проверяем, есть ли уже этот компонент (визуальный (js, css, md), контейнер и перевод).
 // Чего не хватает - создать. Флаги исключают определенные составляющие.
 // Уже созданные составляюие компонентов не перезаписываем.
+
+//
+// логика создания директорий
+// 1 составляющая компонента === один хелпер
+//     makeVisual
+//     makeContainer
+//     makeTranslation
+// создать директрию
+// запустить один или несколько раз хелпер для создания файла (имя файла, хелпер для получения шаблона)
+
+// последовательность: 1 получить параметры -> 2 получить рут -> 3 получить нужные тексты -> 4 создать директории -> 5 создать в них файлы -> 6 записать данные
+// в шагах 4 и 5 проверять, есть ли уже такие директории и файлы, пропускать если уже есть
 
